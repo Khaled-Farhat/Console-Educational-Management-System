f1c68e20d2c4b0b35d2359b8621f162eece7b340
Khaled-Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2019-11-22 22:32:30 +0200

    Update StudentsDatabase.h

--- StudentsDatabase.h
+++ StudentsDatabase.h
@@ -1,4 +1,4 @@
-// StudentsDatabase class defintion.
+// StudentsDatabase class definition.
 #ifndef STUDENTSDATABASE_H
 #define STUDENTSDATABASE_H
 
@@ -13,10 +13,10 @@ public:
 	bool autheticateStudent(const std::string&, const std::string&); // returns true if account authentic
 	bool isStudent(const std::string&); // takes account's username; return true if it is a student
 	void createStudentAccount(const std::string&, const std::string&); // takes username and password; create a new student account
-	void addCourseToStudent(const std::string&, const std::string&); // takes student user name and course's code; add course to student's coruses list
+	void addCourseToStudent(const std::string&, const std::string&); // takes student user name and course's code; add course to student's courses list
 	Student* getStudent(const std::string&); // return pointer to student object
-	void removeCourseFromStudent(const std::string&, const std::string&); // takes student's username and course's code; remove the course from the student's coruses list
-	
+	void removeCourseFromStudent(const std::string&, const std::string&); // takes student's username and course's code; remove the course from the student's courses list
+
 private:
 	std::vector<Student> students;
 };
